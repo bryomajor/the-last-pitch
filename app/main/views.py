ea1050a030e29f38458d7671e68375aacca8f572
Brian Otieno <bryomajor@gmail.com>
2019-10-23 21:41:03 +0300

    Add elements for the root page function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
+from flask_login import login_required, current_user
+from ..models import Pitch, User, Comment, Upvote, Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm
+from flask.views import View, MethodView
+from .. import db
 
 @main.route('/', methods = ['GET', 'POST'])
 def index():
@@ -7,5 +12,12 @@ def index():
     View root page function that returns the index page and it's data
     '''
     title = 'Home'
+    pitch = Pitch.query.filter_by().first()
+    pickuplines = Pitch.query.filter_by(category='pickuplines')
+    interviewpitch = Pitch.query.filter_by(category='interviewpitch')
+    promotionpitch = Pitch.query.filter_by(category='promotionpitch')
+    productpitch = Pitch.query.filter_by(category='productpitch')
 
-    return render_template('index.html', title = title)
+    upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
+
+    return render_template('home.html', title = title, pitch = pitch, pickuplines = pickuplines, interviewpitch = interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch, upvotes = upvotes)
