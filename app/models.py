341cf4208c3f07ac76e2af5352177c72f0b51699
Brian Otieno <bryomajor@gmail.com>
2019-10-23 21:22:53 +0300

    Import the flask login module and implement the login manager

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from . import db
 from sqlalchemy.sql import func
+from . import login_manager
+from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
 
