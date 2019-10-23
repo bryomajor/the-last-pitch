15394947501c12755e3686b744eaf84c3b5ca298
Brian Otieno <bryomajor@gmail.com>
2019-10-23 10:45:07 +0300

    Add configuration classes for the app.

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = 'uncrackablesecretkey'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:p@$$w0rd@localhost/pitches'
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
