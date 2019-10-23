9a11961d1f55beb0bd4b13fb5c88dc094c371d91
Brian Otieno <bryomajor@gmail.com>
2019-10-23 11:10:22 +0300

    Create managers for the app, tests and the shell context

---
+++ manage.py
@@ -0,0 +1,29 @@
+from app import create_app
+from flask_script import Manager, Server
+
+# Creating app instance
+app = create_app('test')
+app = create_app('development')
+app = create_app('production')
+
+# Create manager instance
+manager = Manager(app)
+
+manager.add_command('server', Server)
+
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app)
+
+
+if __name__ == '__main__':
+    manager.run()
