cce4f014081f60cf59ed6341ce7806ffdb7cca5f
Brian Otieno <bryomajor@gmail.com>
2019-10-24 08:54:47 +0300

    Add a view function that renders the registration page

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,3 +18,14 @@ def login():
 
     title = "Last Pitch Login"
     return render_template('auth/login.html', login_form = login_form, title = title)
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = 'New Account'
+    return render_template('auth/register.html', registration_form = form)
