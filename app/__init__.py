f9942401648b027f8d6933fa8d66ec736ccaeecd
Brian Otieno <bryomajor@gmail.com>
2019-10-24 09:31:57 +0300

    Import, instanciate and register login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-
+# Instances of flask extensions
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -23,6 +27,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the main blueprint
     from .main import main as main_blueprint
