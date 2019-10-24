8ce7fe4abc63c2932c6b7f81ef9e7f6d0ceb987a
Brian Otieno <bryomajor@gmail.com>
2019-10-24 22:46:26 +0300

    Create a update_profile view function that takes in a username, and instantiates the UpdateProfile form class

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
 from flask_login import login_required, current_user
 from ..models import Pitch, User, Comment, Upvote, Downvote
-from .forms import PitchForm, CommentForm, UpvoteForm
+from .forms import PitchForm, CommentForm, UpvoteForm, UpdateProfile
 from flask.views import View, MethodView
 from .. import db
 
@@ -89,3 +89,32 @@ def downvote(pitch_id):
     new_downvote = Downvote(pitch_id=pitch_id, user = current_user)
     new_downvote.save_downvotes()
     return redirect(url_for('main.index'))
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html', user = user)
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form = form)
