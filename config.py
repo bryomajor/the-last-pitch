7d1dc1a05deb0ef2c76c6a7b79711e25447ea3e7
Brian Otieno <bryomajor@gmail.com>
2019-10-24 23:29:06 +0300

    Setup the SMTP server and configure the port to use the gmail SMTP server port

--- config.py
+++ config.py
@@ -6,6 +6,14 @@ class Config:
     '''
     SECRET_KEY = 'uncrackablesecretkey'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 
 class ProdConfig(Config):
