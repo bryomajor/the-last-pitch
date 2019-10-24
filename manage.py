3b2db4b686461282481e6dd441264ee8f75dc849
Brian Otieno <bryomajor@gmail.com>
2019-10-24 10:33:55 +0300

    Comment out test and production instances

--- manage.py
+++ manage.py
@@ -4,9 +4,9 @@ from app.models import *
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('test')
+# app = create_app('test')
 app = create_app('development')
-app = create_app('production')
+# app = create_app('production')
 
 # Create manager instance
 manager = Manager(app)
@@ -28,7 +28,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
 
 
 if __name__ == '__main__':
