6db7e554675e5a7c394824015e6d45cad3d0916c
Brian Otieno <bryomajor@gmail.com>
2019-10-24 23:57:41 +0300

    Rectify a module import

--- app/email.py
+++ app/email.py
@@ -1,6 +1,6 @@
 from flask_mail import Message
 from flask import render_template
-from . import flask_mail
+from . import mail
 
 def mail_message(subject,template,to,**kwargs):
     sender_email = bryomajor@gmail.com
